4ce4c96657d86ae91514cf4b08cfe2d6ec276281
talesmilan <100313945+talesmilan@users.noreply.github.com>
2022-09-16 12:04:18 -0300

    Página inicial atualizada

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,14 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
 function Home() {
-    return <h1>Página Inicial</h1>
+    return (<section className={styles.home_container}>
+        <h1>Bem vindo ao <span>Costs</span></h1>
+        <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+        <LinkButton to="/newprojects" text="Criar Projeto" />
+        <img src={savings} alt="Costs"></img>
+    </section>)
 }
 
 export default Home
