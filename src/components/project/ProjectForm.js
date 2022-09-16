d9ef8ceefef62548c405fcafc6e9097e7be02d0d
talesmilan <100313945+talesmilan@users.noreply.github.com>
2022-09-16 20:25:18 -0300

    Começando o backend

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -2,13 +2,27 @@ import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import ButtonSubmit from '../form/SubmitButton'
+import {useState} from 'react'
+import { useEffect } from 'react'
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {    
+        fetch("http://localhost:5000/categories", {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json',},
+    })
+    .then((resp) => resp.json())
+    .then((data) => {setCategories(data)})
+    .catch(err => console.log(err))}, [])
+
     return (
     <form className={styles.form}>
         <Input type="text" text="Nome do Projeto" name="name" placeholder="Insira o nome do projeto" />
         <Input type="number" text="Orçamento do Projeto" name="budget" placeholder="Insira o orçamento total" />
-        <Select name="category_id" text="Selecione a categoria" />
+        <Select name="category_id" text="Selecione a categoria" options={categories} />
         <ButtonSubmit text={btnText} />
     </form>
     )
