e963f3773fd3312e2559c9bae7e6e7b3ba31f811
talesmilan <100313945+talesmilan@users.noreply.github.com>
2022-09-15 22:56:18 -0300

    atualização 15-09

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route,} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Container customClass="min-height">
+            <Route exact path="/"><Home /></Route>
+            <Route path="/company"><Company /></Route>
+            <Route path="/contact"><Contact /></Route>      
+            <Route path="/newproject"><NewProject /></Route> 
+            <Route path="/projects"><Projects /></Route>  
+          </Container>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
