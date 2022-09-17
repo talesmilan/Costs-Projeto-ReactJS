2f4811e7f1754e834406bacd15a883b64f6348e8
talesmilan <100313945+talesmilan@users.noreply.github.com>
2022-09-17 10:16:23 -0300

    Fazendo a mensagem de erro

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,20 @@
+import Message from '../layout/Message'
+import {useLocation} from 'react-router-dom'
+
 function Projects() {
-    return <h1>Projetos</h1>
+
+    const location = useLocation()
+    let message = ''
+    if (location.state) {
+        message = location.state.message
+    }
+
+    return (
+        <div>
+            <h1>Meus Projetos</h1>
+            {message && <Message msg={message} type="success" /> }
+        </div>
+    )
 }
 
 export default Projects
